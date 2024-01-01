74168029bd41bd4b9f3924f9624bc29fbc63fb49
Serb David <83017887+daviddserb@users.noreply.github.com>
2024-01-01 16:39:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,175 +1,181 @@
-const grid = document.getElementById("grid");
-const gameStatus = document.getElementById("gameStatus")
-const width = 9, height = width;
+const GRID = document.getElementById("grid");
+const GRID_SIZE = 9;
+const GAME_STATUS = document.getElementById("gameStatus")
+const SUDOKU_GRID = Array(9).fill().map(() => Array(9).fill());
 
-var squaresMatrix = Array(9).fill().map(() => Array(9).fill());
-var cntTableBuildError = 0;
-var won = false; // Will be true when the player finished/won the game
-var nrTries = 0; // Will check if the player won the game only after some input tries
+var isGameWon = false;
+var cntUserInput = 0;
+var cntSudokuGridBuildError = 0;
 
-function generateBoard() {
-  for (let line = 0; line < width; ++line) {
-    for (let col = 0; col < height; ++col) {
-      var square = document.createElement("textarea");
-      square.setAttribute("id", line + "" + col);
-
-      squaresMatrix[line][col] = square;
-      grid.appendChild(square); // Add the square to the board
-
-      if (line == 2 || line == 5) { // Matrix delimitation (horizontal)
-       square.style.borderBottom = "3px solid";
-      }
-      if (col != 0 && col % 3 == 0) { // Matrix delimitation (vertical)
-        square.style.borderLeft = "3px solid";
-      }
-
-      square.addEventListener("input", function() {
-        validateNumericInput(square);
-      });
-
-      square.addEventListener("click", function() {
-        clickInputSquare(line, col);
-      });
-    }
-  }
-  addNumbers();
-  hideNumbers();
+function startGame() {
+    generateSudokuBoard();
+    fillSudokuBoard();
+    hideNumbersInGrid();
+    createEditableSquares();
 }
 
-function addNumbers() {
-  // Build entire Sudoku table following Sudoku's rules
-  for (let line = 0; line < width; ++line) {
-    for (let col = 0; col < height; ++col) {
-      let randomVal = Math.floor(Math.random() * 9) + 1;
-      if (checkSudokuRules(line, col, randomVal)) {
-        squaresMatrix[line][col].innerHTML = randomVal;
-      } else { // When the random value for the square is not good => check all posibilities from 1 to 9
-        let valueNotGood = "yes", tryAllValues = 1;
-        while (valueNotGood == "yes" && tryAllValues < 10) {
-          if (checkSudokuRules(line, col, tryAllValues)) {
-            valueNotGood = "no";
-            squaresMatrix[line][col].innerHTML = tryAllValues;
-          } else {
-            ++tryAllValues;
-          }
-        }
+function generateSudokuBoard() {
+    for (let row = 0; row < GRID_SIZE; ++row) {
+        for (let col = 0; col < GRID_SIZE; ++col) {
+            var square = document.createElement("textarea");
+            square.setAttribute("id", row + "" + col);
+            SUDOKU_GRID[row][col] = square;
+            grid.appendChild(square); // Add the square to the board
 
-        // When no number [1, 9] is good in the square (extreme case) => delete the entire line and repeat the process from the start of that line
-        if (tryAllValues == 10 && valueNotGood == "yes") {
-          ++cntTableBuildError;
-          for (let colStart = 0; colStart <= col; ++colStart) {
-            squaresMatrix[line][colStart].innerHTML = "";
-          }
-          col = -1;
+            // Matrix delimitation (horizontal)
+            if (row != 0 && row % 3 == 0) square.style.borderTop = "3px solid";
+
+            // Matrix delimitation (vertical)
+            if (col != 0 && col % (GRID_SIZE / 3) == 0) square.style.borderLeft = "3px solid";
+
+            square.addEventListener("click", function() {
+                clickInputSquare(row, col);
+            });
         }
-        // When the table doesn't properly generate (extreme case) => refresh page to don't encounter bugs
-        if (cntTableBuildError == 2000) {
-          location.reload();
-          break;
-        }
-      }
     }
-  }
 }
 
-function checkSudokuRules(lineNr, colNr, valNr) {
-  if (!(1 <= valNr && valNr <= 9)) return false;
+// Add the numbers in Sudoku board following Sudoku's rules
+function fillSudokuBoard() {
+    for (let row = 0; row < GRID_SIZE; ++row) {
+        for (let col = 0; col < GRID_SIZE; ++col) {
+            let randomDigit = Math.floor(Math.random() * 9) + 1;
 
-  // Check the 9x9 matrix line
-  for (let colMatrix = 0; colMatrix < width; ++colMatrix) {
-    if (valNr == squaresMatrix[lineNr][colMatrix].innerHTML && colNr != colMatrix) { //
-      return false;
-    }
-  }
+            // If the digit respects Sudoku's rules => save it
+            if (isDigitValidForSudoku(row, col, randomDigit)) {
+                SUDOKU_GRID[row][col].innerHTML = randomDigit;
+            } else { // If not, check all digits from 1 to 9
+                let firstDigit = 1;
+                const INVALID_NUMBER = 10;
 
-  // Check the 9x9 matrix column
-  for (let lineMatrix = 0; lineMatrix < height; ++lineMatrix) {
-    if (valNr == squaresMatrix[lineMatrix][colNr].innerHTML && lineNr != lineMatrix) { //
-      return false;
-    }
-  }
+                while (firstDigit < 10) {
+                    if (isDigitValidForSudoku(row, col, firstDigit)) {
+                        SUDOKU_GRID[row][col].innerHTML = firstDigit;
+                        break;
+                    } else {
+                        ++firstDigit;
+                    }
+                }
 
-  // Check the 3x3 matrix
-  let startLine3x3Matrix = Math.floor(lineNr / 3) * 3;
-  let startCol3x3Matrix = Math.floor(colNr / 3) * 3;
-  for (let i = startLine3x3Matrix; i < startLine3x3Matrix + 3; ++i) {
-    for (let j = startCol3x3Matrix; j < startCol3x3Matrix + 3; ++j) {
-      if (valNr == squaresMatrix[i][j].innerHTML && lineNr != i && colNr != j) { //
-        return false;
-      }
+                // When no digit between [1, 9] is good in the square (extreme case) => delete the entire row and repeat the process from the start of that row
+                if (firstDigit == INVALID_NUMBER) {
+                    for (let c = 0; c <= col; ++c) {
+                        SUDOKU_GRID[row][c].innerHTML = "";
+                    }
+                    col = -1;
+                    ++cntSudokuGridBuildError;
+                }
+
+                // When the table doesn't properly generate (extreme case) => refresh page to don't encounter bugs
+                // 2000 represents how many times all the digits between [1, 9] didn't fit the square so we restored the entire row
+                if (cntSudokuGridBuildError == 2000) {
+                    location.reload();
+                    break;
+                }
+            }
+        }
     }
-  }
-  return true;
 }
 
-function hideNumbers() {
-  // After the entire sudoku board has been built, then must 'hide' X numbers from random and different positions
-  let numbersToHide = 30;
-  while (numbersToHide != 0) {
-    let line = Math.floor(Math.random() * 9);
-    let column = Math.floor(Math.random() * 9);
-    if (squaresMatrix[line][column].innerHTML != "") {
-      squaresMatrix[line][column].innerHTML = "";
-      --numbersToHide;
+function isDigitValidForSudoku(row, col, digit) {
+    if (!(1 <= digit && digit <= 9)) return false;
+
+    // Check the 9x9 matrix's line - from the start of the line until the position of the new digit
+    for (let bigCol = 0; bigCol < col; ++bigCol) {
+        if (digit == SUDOKU_GRID[row][bigCol].innerHTML) return false;
     }
-  }
-  createEditableSquares();
+
+    // Check the 9x9 matrix's column - from the start of the column until the position of the new digit
+    for (let bigRow = 0; bigRow < row; ++bigRow) {
+        if (digit == SUDOKU_GRID[bigRow][col].innerHTML) return false;
+    }
+
+    // Check the 3x3 matrix excluding the position of the new digit
+    let smallRow = Math.floor(row / 3) * 3;
+    let smallCol = Math.floor(col / 3) * 3;
+    for (let r = smallRow; r < smallRow + 3; ++r) {
+        for (let c = smallCol; c < smallCol + 3; ++c) {
+            if (digit == SUDOKU_GRID[r][c].innerHTML && row != r && col != c) return false;
+        }
+    }
+
+    return true;
+}
+
+// After the entire Sudoku grid has been built, then we must 'hide' CNT_NUMBERS_TO_HIDE amount of numbers from random and different positions
+function hideNumbersInGrid() {
+    const SUDOKU_GRID_POSITIONS = [];
+    const CNT_NUMBERS_TO_HIDE = 30;
+
+    // Create an array of all possible positions in the grid
+    for (let row = 0; row < GRID_SIZE; ++row) {
+        for (let col = 0; col < GRID_SIZE; ++col) {
+            SUDOKU_GRID_POSITIONS.push({ row, col });
+        }
+    }
+
+    const SUDOKU_GRID_POSITIONS_SHUFFLED = SUDOKU_GRID_POSITIONS.sort(() => Math.random() - 0.5);
+
+    // Hide the first 30 positions
+    for (let pos = 0; pos < CNT_NUMBERS_TO_HIDE; ++pos) {
+        const { row, col } = SUDOKU_GRID_POSITIONS_SHUFFLED[pos];
+        SUDOKU_GRID[row][col].innerHTML = "";
+    }
 }
 
 function createEditableSquares() {
-  for (let line = 0; line < width; ++line) {
-    for (let col = 0; col < height; ++col) {
-      if (squaresMatrix[line][col].innerHTML != "") {
-        squaresMatrix[line][col].setAttribute("readonly", true);
-      }
+    for (let row = 0; row < GRID_SIZE; ++row) {
+        for (let col = 0; col < GRID_SIZE; ++col) {
+            if (SUDOKU_GRID[row][col].innerHTML != "") SUDOKU_GRID[row][col].setAttribute("readonly", true);
+        }
     }
-  }
 }
 
-function clickInputSquare(line, col) {
-  if (won) return;
+function clickInputSquare(row, col) {
+    if (isGameWon || SUDOKU_GRID[row][col].readOnly) return;
 
-  if (!squaresMatrix[line][col].readOnly) {
-    ++nrTries;
-    let inputDigit = squaresMatrix[line][col].value;
+    let inputDigit = SUDOKU_GRID[row][col].value;
+    ++cntUserInput;
 
-    if (checkSudokuRules(line, col, inputDigit)) {
-      gameStatus.innerHTML = "Good number.";
-      gameStatus.style.color = "green";
-      squaresMatrix[line][col].innerHTML = inputDigit;
+    if (isDigitValidForSudoku(row, col, inputDigit)) {
+        GAME_STATUS.innerHTML = "Good number.";
+        GAME_STATUS.style.color = "green";
+        SUDOKU_GRID[row][col].innerHTML = inputDigit;
     } else {
-      if (inputDigit != "") {
-        gameStatus.innerHTML = "Bad number.";
-        gameStatus.style.color = "red";
-        squaresMatrix[line][col].innerHTML = "wrong"; // Mark the user's input value as 'wrong' if it doesn't respect Sudoku rules
-      } else { // When user clicks the square or deletes value from square
-        squaresMatrix[line][col].innerHTML = "";
-        gameStatus.innerHTML = "";
-      }
+        if (inputDigit != "") {
+            GAME_STATUS.innerHTML = "Bad number.";
+            GAME_STATUS.style.color = "red";
+            SUDOKU_GRID[row][col].innerHTML = "wrong"; // Mark the user's input value as 'wrong' if it doesn't respect Sudoku rules
+        } else { // When user first time left-clicks the square or deletes the value
+            SUDOKU_GRID[row][col].innerHTML = "";
+            GAME_STATUS.innerHTML = "";
+        }
     }
-  }
 
-  if (nrTries >= 30) checkIfWin();
+    // Check if the game is won only after the initial value of the cntNumbersToHide
+    if (cntUserInput >= 30) checkIfGameWon();
 }
 
-function checkIfWin() {
-  let cnt = 0;
-  for (let i = 0; i < width; ++i) {
-    for (let j = 0; j < height; ++j) {
-      if (squaresMatrix[i][j].innerHTML != "wrong" && squaresMatrix[i][j].innerHTML != "") {
-        ++cnt;
-      }
+function checkIfGameWon() {
+    let cntGoodDigits = 0;
+
+    for (let row = 0; row < GRID_SIZE; ++row) {
+        for (let col = 0; col < GRID_SIZE; ++col) {
+            if (SUDOKU_GRID[row][col].innerHTML != "wrong" && SUDOKU_GRID[row][col].innerHTML != "") {
+                ++cntGoodDigits;
+            }
+        }
     }
-  }
-  if (cnt == 81) {
-    won = true;
-    for (let i = 0; i < width; ++i) {
-      for (let j = 0; j < height; ++j) {
-        squaresMatrix[i][j].setAttribute("readonly", true);
-      }
+
+    if (cntGoodDigits == GRID_SIZE * GRID_SIZE) {
+        for (let row = 0; row < GRID_SIZE; ++row) {
+            for (let col = 0; col < GRID_SIZE; ++col) {
+                SUDOKU_GRID[row][col].setAttribute("readonly", true);
+            }
+        }
+        isGameWon = true;
+        GAME_STATUS.innerHTML = "CONGRATULATIONS! YOU WON!";
     }
-    document.getElementById("gameStatus").innerHTML = "CONGRATULATIONS! YOU WON!";
-  }
 }
 
-generateBoard();
+startGame();
